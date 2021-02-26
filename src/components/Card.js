5cda771c2879ecf626ac02e1c57f361a5c49b8c9
jeromeski <jeromegee@gmail.com>
2021-02-26 19:09:57 +0800

    Implemented smooth scale effects on hover

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import image from '../assets/img/28-450x300.jpg';
 
 const Card = () => {
 	return (
 		<div className='card'>
-			<div className='card__header'></div>
-			<div className='card__body'></div>
-			<div className='card__footer'></div>
+			<div className='card__container'>
+				<div className='card__header'>
+					<div className='card__media'>
+						<div className='card__media-thumb'>
+							<a className='card__hover-effect' href='#'>
+								<img src={image} alt='Rental Place' />
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className='card__body'></div>
+				<div className='card__footer'></div>
+			</div>
 		</div>
 	);
 };
--- src/components/SlickDemo.js
+++ src/components/SlickDemo.js
@@ -9,17 +9,15 @@ class SlickDemo extends Component {
 			infinite: false,
 			speed: 500,
 			slidesToShow: 3,
-			slidesToScroll: 3,
+			slidesToScroll: 1,
 			initialSlide: 0,
-			arrows: false,
+      arrows: false,
 			responsive: [
 				{
 					breakpoint: 1024,
 					settings: {
 						slidesToShow: 3,
-						slidesToScroll: 3,
-						infinite: true,
-						dots: true
+						slidesToScroll: 3
 					}
 				},
 				{
@@ -34,8 +32,7 @@ class SlickDemo extends Component {
 					breakpoint: 480,
 					settings: {
 						slidesToShow: 1,
-						slidesToScroll: 1,
-						arrows: false
+						slidesToScroll: 1
 					}
 				}
 			]
@@ -43,6 +40,11 @@ class SlickDemo extends Component {
 		return (
 			<div className='container'>
 				<Slider {...settings}>
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+					<Card />
 					<Card />
 					<Card />
 					<Card />
